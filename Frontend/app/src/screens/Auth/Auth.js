655ed2a85869b8087d6d05b62855a643e0b6f88d
Sakthi Naveen M <69297600+sakthinaveenm@users.noreply.github.com>
2022-08-25 12:14:36 +0530

    feat : google auth

---
+++ Frontend/app/src/screens/Auth/Auth.js
@@ -0,0 +1,57 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+  } from '@react-native-google-signin/google-signin';
+
+
+  import { styles } from "./styles";
+
+export default function Auth() {
+
+    const {container} = styles;
+
+    GoogleSignin.configure({
+        webClientId:'196301290702-h80qt9mtpeoc1tddqttj83r5725ud27a.apps.googleusercontent.com',
+        offlineAccess: true,
+    })
+    
+
+console.log(container)
+    const signIn = async () => {
+        try {
+          await GoogleSignin.hasPlayServices();
+          const userInfo = await GoogleSignin.signIn();
+          console.log(userInfo)
+        } catch (error) {
+        console.log(error)
+          if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+            // user cancelled the login flow
+          } else if (error.code === statusCodes.IN_PROGRESS) {
+            // operation (e.g. sign in) is in progress already
+          } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+            // play services not available or outdated
+          } else {
+            // some other error happened
+          }
+        }
+      };
+
+
+
+  return (
+    <View style={{...container,padding:10}}>
+       <GoogleSigninButton
+             style={{ width: "100%", height: 60 ,marginTop:10}}
+             size={GoogleSigninButton.Size.Wide}
+             color={GoogleSigninButton.Color.Light}
+            onPress={signIn}
+            />
+    </View>
+  )
+}
